bffdee53e098831866358dd9bc6b89b9ba93d2a6
Ben Cullen <benjaminjcullen1@gmail.com>
2022-07-13 11:05:23 +0100

    added postsReducer and logic to render postLists after axios get

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
-import JSONPlacholder from "../apis/JSONPlacholder";
+import jsonPlacholder from "../apis/JSONPlacholder";
 
 export const fetchPosts = () => async (dispatch) => {
-	const response = await JSONPlacholder.get("/posts");
-	dispatch({ type: "FETCH_POSTS", payload: response });
+	const response = await jsonPlacholder.get("/posts");
+	dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
